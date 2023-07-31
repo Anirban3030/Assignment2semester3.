50a3481f821fe94f7745ca526445a9cef83b42e0
Anirban Mondal <112965065+Anirban3030@users.noreply.github.com>
2023-07-31 16:46:47 +0530

    Update assignment2List(6).py

--- assignment2List(6).py
+++ assignment2List(6).py
@@ -1,5 +1,5 @@
 #6. Remove empty strings from the list of strings. 
-list = ["", "GeeksforGeeks", "", "is", "best", ""]
+list = ["", "Hello", "", "Python", ""]
 print("Original list is : " + str(list))
 while("" in list):
 	list.remove("")
