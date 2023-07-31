d5fa38e3daaeebc028a24d7d366f2ea10893e44b
Anirban Mondal <112965065+Anirban3030@users.noreply.github.com>
2023-07-31 16:59:45 +0530

    Update assignment2List(10).py

--- assignment2List(10).py
+++ assignment2List(10).py
@@ -1,13 +1,13 @@
 #10. Remove all occurrences of a specific item from a list. 
 # Sample list
-my_list = [1, 2, 3, 2, 4, 5, 2]
+list = [1, 2, 3, 2, 4, 5, 2]
 
 # Item to remove
-item_to_remove = 2
+itemtoremove = 2
 
 # Remove all occurrences of the item using the remove() method in a loop
 while item_to_remove in my_list:
-    my_list.remove(item_to_remove)
+    list.remove(itemtoremove)
 
 # Print the updated list
-print("Updated list:", my_list)
+print("Updated list:", list)
