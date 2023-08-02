a004ce3b16e7ac5c2440d49c3c0f13a6acd2605a
Anirban Mondal <112965065+Anirban3030@users.noreply.github.com>
2023-08-03 00:05:25 +0530

    Update assignment2Set(1).py

--- assignment2Set(1).py
+++ assignment2Set(1).py
@@ -1,5 +1,4 @@
 #1. Add a list of elements to a set 
-user_input = input("Enter a list of elements separated by commas: ")
-user_list = user_input.split(',')
-my_set = set(user_list)
-print("The elements have been added to the set:", my_set)
+n = input("Enter a list of elements separated by commas: ")
+list = n.split(',')my_set = set(list)
+print("The elements have been added to the set:", set)
